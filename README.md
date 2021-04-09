ec1aa21744fa3292bf876b40118e1355b8e762c4
shadi-nakhal <lebdroid@gmail.com>
2021-04-09 15:22:26 +0300

    React from scratch

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # React-from-scratch
+
+# install dependancies
+
+## npm install
+
+# start development server on port 3000
+
+## npm start
+
+# build a production bundle
+
+## npm run build
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require("path");
+const HtmlPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.join(__dirname, "/dist"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+  devServer: {
+    port: 3000,
+  },
+};
